1ed9682e10eecdef3c99d443fb435af8b6146077
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-24 22:06:18 +0200

    Fixed wrong number of arguments in customers/new partial

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,2 +1,18 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+describe CustomersController do
+  fixtures :projects
+  render_views #slow, but we don't have view tests
+
+  before do
+    request.session[:user_id] = 1 # login as admin
+    Project.find(1).enable_module!(:customer_module)
+  end
+
+  describe "GET new" do
+    it "renders the form successfully" do
+      get :new, :id => Project.find(1)
+      response.should be_success
+    end
+  end
+end
